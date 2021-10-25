255b03e00be11884bf388d0bda9dc59719151a51
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-25 19:42:25 +0200

    Create & import Reviews_Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Lab1-layout/navbar.js";
 import Header from "./Lab1-layout/header.js";
 import Services_Section from "./Lab1-layout/services-section.js";
 import Work_Section from "./Lab1-layout/work-section.js";
+import Reviews_Section from "./Lab1-layout/reviews-section.js";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Services_Section />
       <Work_Section />
+      <Reviews_Section />
     </div>
   );
 }
