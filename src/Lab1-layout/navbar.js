c0e209ee26e0edc74779adf33a7bf98df09d63d9
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-25 19:39:52 +0200

    Fix navbar className

--- src/Lab1-layout/navbar.js
+++ src/Lab1-layout/navbar.js
@@ -8,28 +8,27 @@ export default class Navbar extends Component {
     render() {
         return ( //in react Figmant container
             <>
-                <nav class="navbar navbar-expand-lg navbar-dark py-3">
-                    <div class="container-fluid">
+                <nav className="navbar navbar-expand-lg navbar-dark py-3">
+                    <div className="container-fluid">
 
-                        <a class="navbar-brand" href="#">
+                        <a className="navbar-brand" href="#">
                             <img src={logoImg} />
                         </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarText">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Features</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Team</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Sign in</a>
+                                </li>
                             </ul>
-                            <span class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
-                            </span>
-                            <span class="nav-item">
-                                <a class="nav-link" href="#">Team</a>
-                            </span>
-                            <span class="nav-item">
-                                <a class="nav-link" href="#">Sign in</a>
-                            </span>
                         </div>
                     </div>
                 </nav>
