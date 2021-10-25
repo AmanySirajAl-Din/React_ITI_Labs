86ec039f516d798de7b2b03f17c993229caf5c9d
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-25 14:52:30 +0200

    Create & import Header functional Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 import Navbar from "./Lab1-layout/navbar.js";
-
+import Header from "./Lab1-layout/header.js";
 
 function App() {
   return (
     <div className="App container">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/Lab1-layout/header.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import logoImg from "./assets/images/logo.svg"
+
+function Header() {
+    return (
+        <header>I'm Header</header>
+
+    );
+}
+
+export default Header;
