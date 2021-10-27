9530a74162178fbd37976f0346bec785f09920d8
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-28 00:36:02 +0200

    fix some styles

--- src/Lab1-layout/navbar.js
+++ src/Lab1-layout/navbar.js
@@ -10,26 +10,20 @@ export default class Navbar extends Component {
             <>
                 <nav className="navbar navbar-expand-lg navbar-dark py-3">
                     <div className="container-fluid">
-
                         <a className="navbar-brand" href="#">
                             <img src={logoImg} />
                         </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Features</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Team</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Sign in</a>
-                                </li>
-                            </ul>
-                        </div>
+                        <ul className="navbar-nav mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Team</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Sign in</a>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </>
