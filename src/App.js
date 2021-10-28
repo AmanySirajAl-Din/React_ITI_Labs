a3df825b68b47206e014ab64b0ea727abe56038a
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-28 08:00:32 +0200

    Create & import RegisterForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import RegisterForm from "./Lab2-register/registerForm";
+
 function App() {
   return (
     <div className="App">
-      Hello React
+      <RegisterForm />
     </div>
   );
 }
---
+++ src/Lab2-register/registerForm.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+export default function RegisterForm() {
+    return (
+        <div className="registerForm">
+            Hello RegisterForm
+        </div>
+    );
+}
